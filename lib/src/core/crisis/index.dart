0a78fa0421214f17ad37e9bd7c3c75a6755c80bc
dgerchcovich <dan.developer789@gmail.com>
2022-11-24 06:15:00 +1100

    Cleanup

--- lib/src/core/crisis/index.dart
+++
@@ -1,8 +0,0 @@
-
-
-typedef CrisisMsg = MsgVerifyInvariant;
-abstract class CrisisMsg {
-   typedef Amino = MsgVerifyInvariant.Amino;
-   typedef Data = MsgVerifyInvariant.Data;
-  typedef Proto = MsgVerifyInvariant.Proto;
-}
