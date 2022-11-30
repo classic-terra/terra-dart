62d112f92bff57d54c1b18a4030070b443c66a56
Dan Gerchcovich <dan.developer789@gmail.com>
2022-12-01 01:41:19 +1100

    Update terraMainDIModule.dart

--- lib/src/Modules/terraMainDIModule.dart
+++ lib/src/Modules/terraMainDIModule.dart
@@ -13,9 +13,11 @@ import 'package:terra_dart/src/Client/Lcd/Api/SlashingAPI.dart';
 import 'package:terra_dart/src/Client/Lcd/Api/StakingAPI.dart';
 import 'package:terra_dart/src/Client/Lcd/Api/TXAPI.dart';
 import 'package:terra_dart/src/Client/Lcd/Api/TendermintAPI.dart';
+import 'package:terra_dart/src/Client/Lcd/Api/TreasuryAPI.dart';
 import 'package:terra_dart/src/Client/Lcd/Api/TxBroadcastApi.dart';
 import 'package:terra_dart/src/Client/Lcd/Api/WasmAPI.dart';
 import 'package:terra_dart/src/Client/Lcd/LCDClient.dart';
+import 'package:terra_dart/src/Client/Lcd/LocalTerra/localTerra.dart';
 
 import '../TerraStartup.dart';
 
@@ -41,6 +43,7 @@ class TerraMainDIModule {
     TerraStartup.injector.registerSingleton<OracleAPI>(() => OracleAPI());
     TerraStartup.injector.registerSingleton<SlashingAPI>(() => SlashingAPI());
     TerraStartup.injector.registerSingleton<StakingAPI>(() => StakingAPI());
+    TerraStartup.injector.registerSingleton<TreasuryAPI>(() => TreasuryAPI());
     TerraStartup.injector
         .registerSingleton<TendermintAPI>(() => TendermintAPI());
     TerraStartup.injector.registerSingleton<TxAPI>(() => TxAPI());
@@ -50,6 +53,66 @@ class TerraMainDIModule {
   }
 
   static void _registerLCDServices() {
-    TerraStartup.injector.registerSingleton<LcdClient>(() => LcdClient());
+    var auth = TerraStartup.injector.get<AuthAPI>();
+    var authz = TerraStartup.injector.get<AuthzAPI>();
+    var bank = TerraStartup.injector.get<BankAPI>();
+    var distribution = TerraStartup.injector.get<DistributionAPI>();
+    var feeGrant = TerraStartup.injector.get<FeeGrantAPI>();
+    var gov = TerraStartup.injector.get<GovAPI>();
+    var ibc = TerraStartup.injector.get<IBCAPI>();
+    var ibcTransfer = TerraStartup.injector.get<IBCTransferAPI>();
+    var marketApi = TerraStartup.injector.get<MarketAPI>();
+    var mint = TerraStartup.injector.get<MintAPI>();
+    var oracle = TerraStartup.injector.get<OracleAPI>();
+    var slashing = TerraStartup.injector.get<SlashingAPI>();
+    var staking = TerraStartup.injector.get<StakingAPI>();
+    var tendermint = TerraStartup.injector.get<TendermintAPI>();
+    var treasury = TerraStartup.injector.get<TreasuryAPI>();
+    var tx = TerraStartup.injector.get<TxAPI>();
+    var txBroadcast = TerraStartup.injector.get<TxBroadcastAPI>();
+    var wasm = TerraStartup.injector.get<WasmAPI>();
+
+    TerraStartup.injector.registerSingleton<LcdClient>(() {
+      return LcdClient(
+          auth,
+          authz,
+          bank,
+          distribution,
+          feeGrant,
+          gov,
+          ibc,
+          ibcTransfer,
+          marketApi,
+          mint,
+          oracle,
+          slashing,
+          staking,
+          tendermint,
+          treasury,
+          tx,
+          txBroadcast,
+          wasm);
+    });
+    TerraStartup.injector.registerSingleton<LocalTerra>(() {
+      return LocalTerra(
+          auth,
+          authz,
+          bank,
+          distribution,
+          feeGrant,
+          gov,
+          ibc,
+          ibcTransfer,
+          marketApi,
+          mint,
+          oracle,
+          slashing,
+          staking,
+          tendermint,
+          treasury,
+          tx,
+          txBroadcast,
+          wasm);
+    });
   }
 }
